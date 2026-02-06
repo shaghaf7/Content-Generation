60228e0026d57dadec1e772701b095cec13c3a8a
shaghaf7 <shaghafkashif@gmail.com>
2026-02-06 23:11:29 +0530

    added blog.java

--- src/main/java/com/example/BlogApp/entity/Blog.java
+++ src/main/java/com/example/BlogApp/entity/Blog.java
@@ -1,4 +1,55 @@
 package com.example.BlogApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "blogs")
 public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String author; // username or email
+
+    public Blog() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+// getters & setters
 }
