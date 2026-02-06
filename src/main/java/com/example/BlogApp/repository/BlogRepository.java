98140135e206656b8ac033b8c503649a4e14b54d
shaghaf7 <shaghafkashif@gmail.com>
2026-02-06 23:12:26 +0530

    blog repository

--- src/main/java/com/example/BlogApp/repository/BlogRepository.java
+++ src/main/java/com/example/BlogApp/repository/BlogRepository.java
@@ -1,4 +1,7 @@
 package com.example.BlogApp.repository;
 
-public interface BlogRepository {
+import com.example.BlogApp.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Blog, Long> {
 }
